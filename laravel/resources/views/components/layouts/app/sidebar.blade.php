aac6ac5d78aa82aa305d468c8c1e0889ce5310c4
darkoVukovic <daredareize@gmail.com>
2025-06-08 15:33:41 +0200

    remove starter kit in navigation

--- laravel/resources/views/components/layouts/app/sidebar.blade.php
+++ laravel/resources/views/components/layouts/app/sidebar.blade.php
@@ -8,7 +8,6 @@
             <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
 
             <a href="{{ route('dashboard') }}" class="me-5 flex items-center space-x-2 rtl:space-x-reverse" wire:navigate>
-                <x-app-logo />
             </a>
 
             <flux:navlist variant="outline">
